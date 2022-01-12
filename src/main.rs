2d0a1c66be1bf76b1a1fb7a862d7c25faff5469f
awkshwayrd <mathur.aksh@gmail.com>
2022-01-12 18:17:16 +0530

    Setup map module

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
 #![warn(clippy::all, clippy::pedantic)]
-use bracket_lib::prelude::*;
+
+mod map;
+mod prelude {
+    pub use bracket_lib::prelude::*;
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+    pub use crate::map::*;
+}
+
+use prelude::*;
 
 struct State {}
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
-        ctx.print(1,1, "Hello, Bracket Terminal!");
+        ctx.print(1, 1, "Hello, Bracket Terminal!");
     }
 }
 
@@ -14,5 +23,5 @@ fn main() -> BError {
     let context = BTermBuilder::simple80x50()
         .with_title("Dungeon Crawler")
         .build()?;
-    main_loop(context, State{})
+    main_loop(context, State {})
 }
