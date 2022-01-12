d0a7e89f13aba09c0d894732d5320032b5cb73f0
awkshwayrd <mathur.aksh@gmail.com>
2022-01-12 17:47:50 +0530

    Bracket Terminal initialized

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
 #![warn(clippy::all, clippy::pedantic)]
-fn main() {
-    println!("Hello, world!");
+use bracket_lib::prelude::*;
+
+struct State {}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print(1,1, "Hello, Bracket Terminal!");
+    }
+}
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Dungeon Crawler")
+        .build()?;
+    main_loop(context, State{})
 }
