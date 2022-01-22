b01e4a8e603e799910bc1e515a84be8df1f29cfc
awkshwayrd <mathur.aksh@gmail.com>
2022-01-22 21:24:37 +0530

    Added monsters using ECS

--- src/spawner.rs
+++ src/spawner.rs
@@ -2,6 +2,7 @@
 
 use crate::prelude::*;
 
+//Add an entity with Player component tag to World
 pub fn spawn_player(ecs: &mut World, pos: Point) {
     ecs.push(
         (
@@ -10,6 +11,29 @@ pub fn spawn_player(ecs: &mut World, pos: Point) {
             Render {
                 color: ColorPair::new(WHITE, BLACK),
                 glyph: to_cp437('@'),
+            }
+        )
+    );
+}
+
+//Add a monster entity with Enemy component tag to World
+pub fn spawn_monster(
+    ecs: &mut World,
+    rng: &mut RandomNumberGenerator,
+    pos: Point
+) {
+    ecs.push(
+        (
+            Enemy,
+            pos,
+            Render {
+                color: ColorPair::new(WHITE, BLACK),
+                glyph: match rng.range(0,4) {
+                    0 => to_cp437('E'), //ettin (a two-headed giant)
+                    1 => to_cp437('O'), //ogre
+                    2 => to_cp437('o'), //orc
+                    _ => to_cp437('g'), //goblin
+                }
             }
         )
     );
