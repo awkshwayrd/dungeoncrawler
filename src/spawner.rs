d1b49ff7d312c6df2ccf6ca5f3b345bf9cd7c869
awkshwayrd <mathur.aksh@gmail.com>
2022-02-06 23:12:21 +0530

    Added hud and tooltips

--- src/spawner.rs
+++ src/spawner.rs
@@ -17,18 +17,28 @@ pub fn spawn_player(ecs: &mut World, pos: Point) {
 
 //Add a monster entity with Enemy component tag to World
 pub fn spawn_monster(ecs: &mut World, rng: &mut RandomNumberGenerator, pos: Point) {
-    ecs.push((
-        Enemy,
-        pos,
-        Render {
-            color: ColorPair::new(WHITE, BLACK),
-            glyph: match rng.range(0, 4) {
-                0 => to_cp437('E'), //ettin (a two-headed giant)
-                1 => to_cp437('O'), //ogre
-                2 => to_cp437('o'), //orc
-                _ => to_cp437('g'), //goblin
+    let (hp, name, glyph) = match rng.roll_dice(1,10) {
+        1..=8 => goblin(),
+        _ => orc()
+    };
+
+    ecs.push(
+        (Enemy,
+            pos,
+            Render{
+                color: ColorPair::new(WHITE, BLACK),
+                glyph,
             },
-        },
         MovingRandomly {},
+        Health{current: hp, max: hp},
+        Name(name)
     ));
+}
+
+fn goblin() -> (i32, String, FontCharType) {
+    (1, "Goblin".to_string(), to_cp437('g'))
+}
+
+fn orc() -> (i32, String, FontCharType) {
+    (2, "Orc".to_string(), to_cp437('o'))
 }
